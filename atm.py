be01c8fcfb326babb956819c07062f2f39cee640
Rajat Jaiswal <rajat.jaiswalmgs2@gmail.com>
2025-08-06 20:09:37 +0530

    fix: updated app.run for Render deployment

--- atm.py
+++ atm.py
@@ -60,6 +60,7 @@ def index():
             message = atm.withdraw(int(amount))
 
     return render_template("atm.html", message=message)
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
